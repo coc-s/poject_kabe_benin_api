c674749db705d93b21d287207503eb1c1e46d2c5
Chebel405 <anne.chebel@gmail.com>
2021-03-23 11:37:05 +0100

    agular

---
+++ kabeBenin/src/app/entity/parrainage.spec.ts
@@ -0,0 +1,7 @@
+import { Parrainage } from './parrainage';
+
+describe('Parrainage', () => {
+  it('should create an instance', () => {
+    expect(new Parrainage()).toBeTruthy();
+  });
+});
---
+++ kabeBenin/src/app/entity/parrainage.ts
@@ -0,0 +1,37 @@
+export class Parrainage {
+    id: number;
+    nomEnfant: string;
+    prenomEnfant: string;
+    dateNaissEnfant: Date;
+    sexe: string;
+    dateParrainage: string;
+    ecole: string;
+    village: string;
+
+
+
+    constructor(id: number,
+
+        nomEnfant: string,
+        prenomEnfant: string,
+        dateNaissEnfant: Date,
+        sexe: string,
+        dateParrainage: string,
+        ecole: string,
+        village: string) {
+
+        this.id = id;
+        this.nomEnfant = nomEnfant;
+        this.prenomEnfant = prenomEnfant;
+        this.dateNaissEnfant = dateNaissEnfant;
+        this.sexe = sexe;
+        this.dateParrainage = dateParrainage;
+        this.ecole = ecole
+        this.village = village;
+    
+
+
+}
+
+
+}
