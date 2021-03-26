bfa74bf7915002945716c0316e0c1d38c98aded5
GregoryBizeau <gregory.bizeau@gmail.com>
2021-03-26 11:51:04 +0100

    bug2

--- kabeBenin/src/app/projet/projet.component.ts
+++ kabeBenin/src/app/projet/projet.component.ts
@@ -1,17 +1,98 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
+
   selector: 'app-projet',
+
   templateUrl: './projet.component.html',
+
   styleUrls: ['./projet.component.scss']
+
 })
+
 export class ProjetComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+
+  myIndex;
+
+  x;
+
+  constructor() {
+
+
+
+    this.myIndex=0
+
+    this.x= document.getElementsByClassName("mySlides");
+
+
+
   }
 
-  
+
+
+  ngOnInit(): void {
+
+
+
+    this.carousel()
+
+
+
+  }
+
+
+
+   carousel() {
+
+
+
+    var i;
+
+
+
+
+    for (i = 0; i < this.x.length; i++) {
+
+      this.x[i].setAttribute('style','display: none;')
+
+      console.log(this.x[i])
+
+    }
+
+    this.myIndex++;
+
+    if ( this.myIndex > this.x.length) {
+
+      this.myIndex = 1
+
+    }
+
+   this.x[this.myIndex-1].setAttribute('style','display: block; width:50%')
+
+    console.log(this.myIndex)
+
+    //setTimeout(this.carousel, 3000); // Change image every 2 seconds
+
+    setTimeout(
+
+      () => {
+
+       this.carousel();
+
+      }, 3000
+
+    );
+
+
+
+
+}
+
+
 
 }
