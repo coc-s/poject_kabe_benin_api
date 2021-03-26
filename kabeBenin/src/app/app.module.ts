3d14b2f1e0e19c7025645ef5d2a23fc238391fda
parmsa <parmentier.samuel@sfr.fr>
2021-03-26 19:21:38 +0100

    voila voila

--- kabeBenin/src/app/app.module.ts
+++ kabeBenin/src/app/app.module.ts
@@ -5,45 +5,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InterfaceLoginComponent } from './interface-login/interface-login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from './service/user.service';
-import { ParrainageService } from './service/parrainage.service';
-import { CreateParrainageComponent } from './parrainage/create/create.component';
-import { CreateProduitComponent } from './produit/create/create.component';
-import { CreateProjetHumanitaireComponent } from './projet-humanitaire/create/create.component';
-import { ProjetHumanitaireService } from './service/projet-humanitaire.service';
-import { CreateBanqueAssociationComponent } from './banqueAssociation/create/create.component';
-import { BanqueAssociationService } from './service/banque-association.service';
-import { ProduitService } from './service/produit.service';
-import { CreateComponent } from './utilisateurs/create/create.component';
+import { AdminComponent } from './admin/admin.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { MenuAccueilComponent } from './menu-accueil/menu-accueil.component';
+import { VillageKabeComponent } from './village-kabe/village-kabe.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { FaireUnDonComponent } from './faire-un-don/faire-un-don.component';
+import { NousContacterComponent } from './nous-contacter/nous-contacter.component';
+import { PaiementDonComponent } from './paiement-don/paiement-don.component';
+import { ProjetComponent } from './projet/projet.component';
+import { VenteComponent } from './vente/vente.component';
+import { EvenementComponent } from './evenement/evenement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InterfaceLoginComponent,
-    CreateComponent, 
-    CreateParrainageComponent ,
-    CreateProduitComponent,
-    CreateProjetHumanitaireComponent,
-    CreateBanqueAssociationComponent
+    InscriptionComponent,
+    AdminComponent,
+    AccueilComponent,
+    MenuAccueilComponent,
+    VillageKabeComponent,
+    ConnexionComponent,
+    FaireUnDonComponent,
+    NousContacterComponent,
+    PaiementDonComponent,
+    ProjetComponent,
+    VenteComponent,
+    EvenementComponent
   ],
   imports: [
+    
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [
-  UserService,
-  ParrainageService,
-  ProduitService,
-  ProjetHumanitaireService,
-  BanqueAssociationService
-
-],
-
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
